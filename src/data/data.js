51d0aa76fd8085eea3cfbb15dc424538471eee17
rychersky <rychersky@gmail.com>
2024-01-17 13:25:47 -0600

    finished

--- src/data/data.js
+++ src/data/data.js
@@ -2,38 +2,71 @@ import * as dayjs from 'dayjs';
 
 export async function getCurrentWeather(locationString) {
   try {
-    const baseUrl = 'https://api.weatherapi.com/v1/current.json';
+    const baseUrl = 'https://api.weatherapi.com/v1/forecast.json';
     const encodedLocation = encodeURIComponent(locationString);
+    const days = 3;
     const key = '973488068c634787b66233221240801';
-    const url = `${baseUrl}?q=${encodedLocation}&key=${key}`;
-    const response = await fetch(url, { mode: 'cors' });
+    const url = `${baseUrl}?q=${encodedLocation}&key=${key}&days=${days}`;
+    const response = await fetch(url, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      mode: 'cors',
+    });
+
     if (response.ok) {
       const json = await response.json();
-      const weather = {};
-      weather.name = json.location.name;
-      weather.region = json.location.region;
-      weather.country = json.location.country;
-      weather.img = `https:${json.current.condition.icon}`;
-      weather.temperature = {
-        f: Math.round(json.current.temp_f),
-        c: Math.round(json.current.temp_c),
+      const current = buildCurrentData(json);
+      const forecast = buildForecastData(json);
+      return { current, forecast, error: '' };
+    } else {
+      return {
+        current: undefined,
+        forecast: undefined,
+        error: 'No matching location found.',
       };
-      weather.humidity = json.current.humidity;
-      weather.feelsLike = {
-        f: Math.round(json.current.feelslike_f),
-        c: Math.round(json.current.feelslike_c),
-      };
-      weather.wind = {
-        mph: Math.round(json.current.wind_mph),
-        kph: Math.round(json.current.wind_kph),
-      };
-      const timeNum = Number(`${json.current.last_updated_epoch}000`);
-      weather.dayTime = dayjs(timeNum).format('dddd h:mm A');
-      weather.description = json.current.condition.text;
-
-      return weather;
     }
   } catch (e) {
-    console.log(e);
+    console.log(`Something went wrong 😭: ${e}`);
   }
+}
+
+function buildCurrentData(json) {
+  return {
+    name: json.location.name,
+    region: json.location.region,
+    country: json.location.country,
+    img: `https:${json.current.condition.icon}`,
+    temperature: {
+      f: Math.round(json.current.temp_f),
+      c: Math.round(json.current.temp_c),
+    },
+    humidity: json.current.humidity,
+    feelsLike: {
+      f: Math.round(json.current.feelslike_f),
+      c: Math.round(json.current.feelslike_c),
+    },
+    wind: {
+      mph: Math.round(json.current.wind_mph),
+      kph: Math.round(json.current.wind_kph),
+    },
+    dayTime: dayjs(json.current.last_updated_epoch * 1000).format(
+      'dddd h:mm A'
+    ),
+    description: json.current.condition.text,
+  };
+}
+
+function buildForecastData(json) {
+  return json.forecast.forecastday.map((day) => {
+    return {
+      day: dayjs(day.date_epoch * 1000).format('dddd'),
+      maxtemp_c: Math.round(day.day.maxtemp_c),
+      maxtemp_f: Math.round(day.day.maxtemp_f),
+      mintemp_c: Math.round(day.day.mintemp_c),
+      mintemp_f: Math.round(day.day.mintemp_f),
+      img: `https:${day.day.condition.icon}`,
+      description: day.day.condition.text,
+    };
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,51 +1,132 @@
 import './main.scss';
 import { getCurrentWeather } from './data/data';
 
-let weather;
+let weatherCache;
 
 const form = document.querySelector('.search');
 const input = document.querySelector('#search-bar');
-const submit = document.querySelector('.search button');
+const submitBtn = document.querySelector('.search button');
+const errorMsg = document.querySelector('.search p');
 
+const resultsContainer = document.querySelector('.results');
+const name = resultsContainer.querySelector('.name');
+const region = resultsContainer.querySelector('.region');
+const country = resultsContainer.querySelector('.country');
+const img = resultsContainer.querySelector('.current-left img');
+const temperature = resultsContainer.querySelector('.temperature');
 const fahrenheit = document.querySelector('.fahrenheit');
-
-const name = document.querySelector('.name');
-const region = document.querySelector('.region');
-const country = document.querySelector('.country');
-const img = document.querySelector('.current-left img');
-const temperature = document.querySelector('.temperature');
-const humidity = document.querySelector('.humidity span');
-const feelsLike = document.querySelector('.feels-like span');
-const wind = document.querySelector('.wind-value');
-const dayTime = document.querySelector('.day-time');
-const description = document.querySelector('.description');
+const celcius = document.querySelector('.celcius');
+const humidity = resultsContainer.querySelector('.humidity span');
+const feelsLike = resultsContainer.querySelector('.feels-like span');
+const windValue = resultsContainer.querySelector('.wind-value');
+const windUnits = resultsContainer.querySelector('.wind-units');
+const dayTime = resultsContainer.querySelector('.day-time');
+const description = resultsContainer.querySelector('.description');
+const forecast = resultsContainer.querySelector('.forecast');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
+  errorMsg.innerHTML = '';
   input.disabled = true;
-  submit.disabled = true;
+  submitBtn.disabled = true;
 
-  const isFahrenheit = fahrenheit.disabled;
-  weather = await getCurrentWeather(input.value);
+  weatherCache = await getCurrentWeather(input.value);
 
-  name.innerHTML = weather.name;
-  region.innerHTML = weather.region;
-  country.innerHTML = weather.country;
-  img.src = weather.img;
-  temperature.innerHTML = fahrenheit.disabled
-    ? weather.temperature.c
-    : weather.temperature.f;
-  humidity.innerHTML = weather.humidity;
-  feelsLike.innerHTML = fahrenheit.disabled
-    ? weather.feelsLike.c
-    : weather.feelsLike.f;
-  wind.innerHTML = fahrenheit.disabled ? weather.wind.kph : weather.wind.mph;
-  dayTime.innerHTML = weather.dayTime;
-  description.innerHTML = weather.description;
+  if (weatherCache.current) {
+    resultsContainer.style.display = 'block';
+    buildCurrent();
+    buildForecast();
+  } else {
+    errorMsg.innerHTML = weatherCache.error;
+  }
 
   input.disabled = false;
-  submit.disabled = false;
-  console.log(weather);
+  submitBtn.disabled = false;
+});
+
+function buildCurrent() {
+  name.innerHTML = weatherCache.current.name;
+  region.innerHTML = weatherCache.current.region
+    ? `${weatherCache.current.region},`
+    : '';
+  country.innerHTML = weatherCache.current.country;
+  img.src = weatherCache.current.img;
+  temperature.innerHTML = fahrenheit.disabled
+    ? weatherCache.current.temperature.f
+    : weatherCache.current.temperature.c;
+  humidity.innerHTML = weatherCache.current.humidity;
+  feelsLike.innerHTML = fahrenheit.disabled
+    ? weatherCache.current.feelsLike.f
+    : weatherCache.current.feelsLike.c;
+  windValue.innerHTML = fahrenheit.disabled
+    ? weatherCache.current.wind.mph
+    : weatherCache.current.wind.kph;
+  windUnits.innerHTML = fahrenheit.disabled ? 'mph' : 'kph';
+  dayTime.innerHTML = weatherCache.current.dayTime;
+  description.innerHTML = weatherCache.current.description;
+}
+
+function buildForecast() {
+  forecast.innerHTML = '';
+  weatherCache.forecast.forEach((day) => {
+    const dayDiv = document.createElement('div');
+    dayDiv.classList.add('forecast-day');
+    dayDiv.innerHTML = /* html */ `
+      <p class="forecast-day-label">${day.day}</p>
+      <img src=${day.img} />
+      <div>
+        <p class="forecast-high"><span>${
+          fahrenheit.disabled ? day.maxtemp_f : day.maxtemp_c
+        }</span>°</p>
+        <p class="forecast-low"><span>${
+          fahrenheit.disabled ? day.mintemp_f : day.mintemp_c
+        }</span>°</p>
+      </div>
+    `;
+    forecast.appendChild(dayDiv);
+  });
+}
+
+fahrenheit.addEventListener('click', () => {
+  fahrenheit.disabled = true;
+  celcius.disabled = false;
+  temperature.innerHTML = weatherCache.current.temperature.f;
+  feelsLike.innerHTML = weatherCache.current.feelsLike.f;
+  windValue.innerHTML = weatherCache.current.wind.mph;
+  windUnits.innerHTML = 'mph';
+
+  const forecastDays = document.querySelectorAll('.forecast-day');
+  forecastDays.forEach((day) => {
+    const dayName = day.querySelector('.forecast-day-label').innerHTML;
+    const dayHigh = day.querySelector('.forecast-high span');
+    const dayLow = day.querySelector('.forecast-low span');
+    const dayWeather = weatherCache.forecast.find(
+      (cacheDay) => cacheDay.day === dayName
+    );
+    dayHigh.innerHTML = dayWeather.maxtemp_f;
+    dayLow.innerHTML = dayWeather.mintemp_f;
+  });
+});
+
+celcius.addEventListener('click', () => {
+  celcius.disabled = true;
+  fahrenheit.disabled = false;
+  temperature.innerHTML = weatherCache.current.temperature.c;
+  feelsLike.innerHTML = weatherCache.current.feelsLike.c;
+  windValue.innerHTML = weatherCache.current.wind.kph;
+  windUnits.innerHTML = 'kph';
+
+  const forecastDays = document.querySelectorAll('.forecast-day');
+  forecastDays.forEach((day) => {
+    const dayName = day.querySelector('.forecast-day-label').innerHTML;
+    const dayHigh = day.querySelector('.forecast-high span');
+    const dayLow = day.querySelector('.forecast-low span');
+    const dayWeather = weatherCache.forecast.find(
+      (cacheDay) => cacheDay.day === dayName
+    );
+    dayHigh.innerHTML = dayWeather.maxtemp_c;
+    dayLow.innerHTML = dayWeather.mintemp_c;
+  });
 });
 
 const holder = input.value;
